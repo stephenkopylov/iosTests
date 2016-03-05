1993be695bbe9dda17b87d8bdc307b850773efe4
Stephen Kopylov <stephen@expertoption.com>
2016-03-05 20:15:13 +0200

    OAStackViewTest added

---
+++ OAStackViewTest/OAStackViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OAStackViewTest
+//
+//  Created by Stephen Kopylov - Home on 05/03/16.
+//  Copyright © 2016 Stephen Kopylov - Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OAStackViewTest/OAStackViewTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OAStackViewTest
+//
+//  Created by Stephen Kopylov - Home on 05/03/16.
+//  Copyright © 2016 Stephen Kopylov - Home. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
