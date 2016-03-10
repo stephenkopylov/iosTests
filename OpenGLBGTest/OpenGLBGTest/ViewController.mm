b43a7bb2970967af3334d7c3603627c3b2039173
Stephen Kopylov - Home <stephen@expertoption.com>
2016-03-11 01:32:49 +0200

    Some progress

--- OpenGLBGTest/OpenGLBGTest/ViewController.mm
+++ OpenGLBGTest/OpenGLBGTest/ViewController.mm
@@ -6,8 +6,13 @@
 //  Copyright © 2016 Stephen Kopylov - Home. All rights reserved.
 //
 
-#import "ViewController.h"
+#define NANOVG_GLES2_IMPLEMENTATION
 #import <OpenGLES/ES2/glext.h>
+#include "nvg/nanovg.h"
+#include "nvg/nanovg_gl.h"
+#include "nvg/nanovg_gl_utils.h"
+#import "ViewController.h"
+
 
 @interface ViewController ()
 @property (nonatomic, strong) CAEAGLLayer *renderLayer;
@@ -18,6 +23,7 @@
 @property (nonatomic) CADisplayLink *displayLink;
 @property (nonatomic) GLuint colorRenderBuffer;
 @property (nonatomic) EAGLSharegroup *sharegroup;
+@property (nonatomic) NVGcontext *vg;
 
 @end
 
@@ -49,6 +55,7 @@
     self.renderQueue = dispatch_queue_create("Render-Queue", DISPATCH_QUEUE_SERIAL);
     
     self.mainContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [EAGLContext setCurrentContext:self.mainContext];
     GLKView *view = (GLKView *)self.view;
     view.context = self.mainContext;
     [self.mainContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.view.layer];
@@ -64,6 +71,8 @@
         glGenFramebuffers(1, &framebuffer);
         glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
         glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _colorRenderBuffer);
+        
+        _vg = nvgCreateGLES2(NVG_STENCIL_STROKES | NVG_DEBUG);
     });
 }
 
@@ -72,9 +81,21 @@
 {
     dispatch_async(self.renderQueue, ^{
         [EAGLContext setCurrentContext:self.renderContext];
+        
         glViewport(0, 0, 100.0, 100.0);
         glClearColor(10.0, 104.0 / 255.0, 55.0 / 255.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
+        nvgBeginFrame(_vg, 100.0, 100.0, 2.0);
+        
+        nvgStrokeColor(_vg, nvgRGB(1.0, 0.5, 1.0));
+        nvgStrokeWidth(_vg, 1.5f);
+        
+        nvgBeginPath(_vg);
+        nvgMoveTo(_vg, 0.0, 0.0);
+        nvgLineTo(_vg, 100.0, 100.0);
+        nvgStroke(_vg);
+        
+        nvgEndFrame(_vg);
         
         dispatch_sync(dispatch_get_main_queue(), ^{
             [EAGLContext setCurrentContext:self.mainContext];
