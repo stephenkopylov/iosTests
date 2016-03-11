33ec91ac54b1aea3319faf5bd2ce6cf84cb3347c
Stephen Kopylov - Home <stephen@expertoption.com>
2016-03-11 02:15:34 +0200

    Some progress

--- OpenGLBGTest/OpenGLBGTest/ViewController.mm
+++ OpenGLBGTest/OpenGLBGTest/ViewController.mm
@@ -60,8 +60,11 @@
     GLKView *view = (GLKView *)self.view;
     view.context = self.mainContext;
     
+    [EAGLContext setCurrentContext:self.mainContext];
     
+    _vg = nvgCreateGLES2(NVG_STENCIL_STROKES | NVG_DEBUG);
     dispatch_async(self.renderQueue, ^{
+        /*
         _renderLayer = [CAEAGLLayer new];
         _renderLayer.frame = CGRectMake(0.0, 0.0, 100., 100.0);
         _renderLayer.opaque = YES;
@@ -81,8 +84,8 @@
         glGenFramebuffers(1, &_frameBuffer);
         glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer);
         glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _colorRenderBuffer);
+        */
         
-        _vg = nvgCreateGLES2(NVG_STENCIL_STROKES | NVG_DEBUG);
     });
 }
 
@@ -91,7 +94,7 @@
 {
     dispatch_async(self.renderQueue, ^{
         [EAGLContext setCurrentContext:self.renderContext];
-        
+        /*
         glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
         glBindRenderbuffer(GL_FRAMEBUFFER, _frameBuffer);
         
@@ -101,27 +104,27 @@
         glClear(GL_COLOR_BUFFER_BIT);
         
         [self.renderContext presentRenderbuffer:GL_RENDERBUFFER];
-        
-        nvgBeginFrame(_vg, 100.0, 100.0, 2.0);
-        
-        nvgStrokeColor(_vg, nvgRGB(1.0, 0.5, 1.0));
-        nvgStrokeWidth(_vg, 1.5f);
-        
-        nvgBeginPath(_vg);
-        nvgMoveTo(_vg, 0.0, 0.0);
-        nvgLineTo(_vg, 100.0, 100.0);
-        nvgStroke(_vg);
-        
-        nvgEndFrame(_vg);
+        */
+
         
         
         dispatch_sync(dispatch_get_main_queue(), ^{
             [EAGLContext setCurrentContext:self.mainContext];
             
-            glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
-            
             glClearColor(10.0 / 255.0, 10.0 / 255.0, 100.0 / 255.0, 0.0);
             glClear(GL_COLOR_BUFFER_BIT);
+            
+            nvgBeginFrame(_vg, 100.0, 100.0, 2.0);
+            
+            nvgStrokeColor(_vg, nvgRGB(1.0, 0.5, 1.0));
+            nvgStrokeWidth(_vg, 1.5f);
+            
+            nvgBeginPath(_vg);
+            nvgMoveTo(_vg, 0.0, 0.0);
+            nvgLineTo(_vg, 100.0, 100.0);
+            nvgStroke(_vg);
+            
+            nvgEndFrame(_vg);
             
             [self.mainContext presentRenderbuffer:GL_RENDERBUFFER];
         });
