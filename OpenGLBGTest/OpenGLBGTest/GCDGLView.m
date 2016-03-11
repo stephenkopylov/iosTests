ad499ccfe768e4e2dbccb8aeaa2332284b2bead0
Stephen Kopylov - Home <stephen@expertoption.com>
2016-03-11 18:40:47 +0200

    Such progress

--- OpenGLBGTest/OpenGLBGTest/GCDGLView.m
+++ OpenGLBGTest/OpenGLBGTest/GCDGLView.m
@@ -7,9 +7,9 @@
 //
 #import "GCDGLView.h"
 #import <OpenGLES/ES2/glext.h>
+#include <OpenGLES/ES2/gl.h>
 
 @interface GCDGLView ()
-@property (nonatomic, strong) CAEAGLLayer *mainLayer;
 @property (nonatomic, strong) dispatch_queue_t renderQueue;
 @property (nonatomic, strong) EAGLContext *renderContext;
 @property (nonatomic, strong) EAGLContext *mainContext;
@@ -53,6 +53,12 @@
 }
 
 
++ (Class)layerClass
+{
+    return [CAEAGLLayer class];
+}
+
+
 - (void)setup
 {
     self.renderLock = [[NSLock alloc] init];
@@ -60,55 +66,41 @@
     self.mainContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     [EAGLContext setCurrentContext:self.mainContext];
     
-    self.mainLayer = [CAEAGLLayer new];
-    self.mainLayer.frame = CGRectMake(0, 0, 100, 100);
-    self.mainLayer.opaque = NO;
-    self.mainLayer.contentsScale = [UIScreen mainScreen].scale;
-    [self.layer addSublayer:self.mainLayer];
+    ((CAEAGLLayer *)self.layer).contentsScale = [UIScreen mainScreen].scale;
     
     glGenRenderbuffers(1, &_renderbuffer);
     glBindRenderbuffer(GL_RENDERBUFFER, _renderbuffer);
-
-    GLint max_rb_size;
-    glGetIntegerv (GL_MAX_RENDERBUFFER_SIZE, &max_rb_size);
     
     glGenRenderbuffers(1, &_stencilbuffer);
     glBindRenderbuffer(GL_RENDERBUFFER, _stencilbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, max_rb_size, max_rb_size);
     
     glGenFramebuffers(1, &_framebuffer);
     glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _renderbuffer);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _stencilbuffer);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _stencilbuffer);
     
     glBindRenderbuffer(GL_RENDERBUFFER, _renderbuffer);
     
     GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
     
-    if(status == GL_FRAMEBUFFER_COMPLETE)
-    {
+    if ( status == GL_FRAMEBUFFER_COMPLETE ) {
         NSLog(@"framebuffer complete");
         //NSLog(@"failed to make complete framebuffer object %x", status);
     }
-    else if(status == GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT)
-    {
+    else if ( status == GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT ) {
         NSLog(@"incomplete framebuffer attachments");
     }
-    else if(status == GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT)
-    {
+    else if ( status == GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT ) {
         NSLog(@"incomplete missing framebuffer attachments");
     }
-    else if(status == GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS)
-    {
+    else if ( status == GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS ) {
         NSLog(@"incomplete framebuffer attachments dimensions");
     }
-    else if(status == GL_FRAMEBUFFER_UNSUPPORTED)
-    {
+    else if ( status == GL_FRAMEBUFFER_UNSUPPORTED ) {
         NSLog(@"combination of internal formats used by attachments in thef ramebuffer results in a nonrednerable target");
     }
     
-    [self.mainContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:self.mainLayer];
+    [self.mainContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.layer];
     
     dispatch_async(self.renderQueue, ^{
         self.renderContext = [[EAGLContext alloc] initWithAPI:self.mainContext.API sharegroup:self.mainContext.sharegroup];
@@ -119,23 +111,8 @@
         }
     });
     
-    if(self.displayLink){
-        [self.displayLink invalidate];
-    }
-    
     self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render)];
     [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
-    
-
-}
-
-
-- (void)layoutSublayersOfLayer:(CALayer *)layer
-{
-    [super layoutSublayersOfLayer:layer];
-    [EAGLContext setCurrentContext:self.mainContext];
-    self.mainLayer.frame = self.layer.frame;
-    [self.mainContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:self.mainLayer];
 }
 
 
@@ -150,11 +127,18 @@
     
     CGRect frame =  self.layer.frame;
     CGFloat scale = [UIScreen mainScreen].scale;
+    [EAGLContext setCurrentContext:self.mainContext];
+    [self.mainContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.layer];
+    
     dispatch_async(self.renderQueue, ^{
         [self.renderLock lock];
         [EAGLContext setCurrentContext:self.renderContext];
-        glBindRenderbuffer(GL_RENDERBUFFER, _renderbuffer);
+        
         glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
+        glBindRenderbuffer(GL_RENDERBUFFER, _stencilbuffer);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, frame.size.width * scale, frame.size.height * scale);
+        glBindRenderbuffer(GL_RENDERBUFFER, _renderbuffer);
+        
         glViewport(0, 0, frame.size.width * scale, frame.size.height * scale);
         glClearColor(0.f, 0.f, 0.5f, 0.3);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
