11a17f51ccef7192160cf26a9aefc6da5044f867
Stephen Kopylov - Home <stephen@expertoption.com>
2016-03-11 18:03:49 +0200

    Some progress

--- OpenGLBGTest/OpenGLBGTest/GCDGLView.m
+++ OpenGLBGTest/OpenGLBGTest/GCDGLView.m
@@ -69,15 +69,18 @@
     glGenRenderbuffers(1, &_renderbuffer);
     glBindRenderbuffer(GL_RENDERBUFFER, _renderbuffer);
 
+    GLint max_rb_size;
+    glGetIntegerv (GL_MAX_RENDERBUFFER_SIZE, &max_rb_size);
+    
     glGenRenderbuffers(1, &_stencilbuffer);
     glBindRenderbuffer(GL_RENDERBUFFER, _stencilbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, 1024, 1024);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, max_rb_size, max_rb_size);
     
     glGenFramebuffers(1, &_framebuffer);
     glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _renderbuffer);
-    //glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _stencilbuffer);
-    //glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _stencilbuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _stencilbuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _stencilbuffer);
     
     glBindRenderbuffer(GL_RENDERBUFFER, _renderbuffer);
     
@@ -116,6 +119,13 @@
         }
     });
     
+    if(self.displayLink){
+        [self.displayLink invalidate];
+    }
+    
+    self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render)];
+    [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+    
 
 }
 
@@ -126,13 +136,6 @@
     [EAGLContext setCurrentContext:self.mainContext];
     self.mainLayer.frame = self.layer.frame;
     [self.mainContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:self.mainLayer];
-    
-    if(self.displayLink){
-        [self.displayLink invalidate];
-    }
-    
-    self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render)];
-    [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
 }
 
 
@@ -147,29 +150,18 @@
     
     CGRect frame =  self.layer.frame;
     CGFloat scale = [UIScreen mainScreen].scale;
-    
     dispatch_async(self.renderQueue, ^{
         [self.renderLock lock];
         [EAGLContext setCurrentContext:self.renderContext];
-        
         glBindRenderbuffer(GL_RENDERBUFFER, _renderbuffer);
         glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
-        
         glViewport(0, 0, frame.size.width * scale, frame.size.height * scale);
         glClearColor(0.f, 0.f, 0.5f, 0.3);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        glClearStencil(0);
-        glEnable(GL_STENCIL_TEST);
-        
-        glStencilFunc(GL_ALWAYS, 1, 1);
-        glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
-        
         if ( [self.delegate respondsToSelector:@selector(drawInRect:forView:)] ) {
             [self.delegate drawInRect:frame forView:self];
         }
-        
-        glDisable(GL_STENCIL_TEST);
         
         glFlush();
         glBindRenderbuffer(GL_RENDERBUFFER, 0);
