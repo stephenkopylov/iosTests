fe9e6807513953cfb08c980f37c11b27e56b9df7
Stephen Kopylov <stephen@expertoption.com>
2016-04-27 20:16:13 +0300

    nanovg additions

--- SKAsyncGLViewController/SKAsyncGLViewController/SampleViewController.m
+++ SKAsyncGLViewController/SKAsyncGLViewController/SampleViewController.m
@@ -5,18 +5,25 @@
 //  Created by Stephen Kopylov - Home on 27/04/16.
 //  Copyright © 2016 test. All rights reserved.
 //
-
+#define NANOVG_GLES2_IMPLEMENTATION
 #import "SampleViewController.h"
 #import <OpenGLES/ES2/glext.h>
 #import <OpenGLES/ES2/gl.h>
+#import "nanovg/nanovg.c"
+#import "nanovg/nanovg.h"
+#import "nanovg/nanovg_gl.h"
+#import "nanovg/nanovg_gl_utils.h"
 
 @interface SampleViewController ()<SKAsyncGLViewControllerDelegate>
 
 @property (nonatomic) GLuint stencilbuffer;
-
 @property (nonatomic) GLuint sampleframebuffer;
 @property (nonatomic) GLuint samplestencilbuffer;
 @property (nonatomic) GLuint samplerenderbuffer;
+
+@property (nonatomic)  NVGcontext *vg;
+
+@property (nonatomic) CGRect savedRect;
 
 @end
 
@@ -50,10 +57,16 @@
 }
 
 
-- (void)updateBuffersSize
+- (void)updateBuffersSize:(CGRect)rect
 {
-    CGFloat width = self.view.frame.size.width * [UIScreen mainScreen].scale;
-    CGFloat height = self.view.frame.size.height * [UIScreen mainScreen].scale;
+    if ( CGRectEqualToRect(rect, _savedRect)) {
+        return;
+    }
+    
+    _savedRect = rect;
+    
+    CGFloat width = rect.size.width;
+    CGFloat height = rect.size.height;
     
     glBindRenderbuffer(GL_RENDERBUFFER, _stencilbuffer);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, width, height);
@@ -83,14 +96,14 @@
     glGenRenderbuffers(1, &_samplerenderbuffer);
     glGenRenderbuffers(1, &_samplestencilbuffer);
     
-    [self updateBuffersSize];
+    [self updateBuffersSize:CGRectMake(0.0f, 0.0f, self.view.frame.size.width *[UIScreen mainScreen].scale, self.view.frame.size.height *[UIScreen mainScreen].scale)];
     
     glGenFramebuffers(1, &_sampleframebuffer);
     glBindFramebuffer(GL_FRAMEBUFFER, _sampleframebuffer);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _samplerenderbuffer);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _samplestencilbuffer);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _samplestencilbuffer);
-    
+    self.vg = nvgCreateGLES2(NVG_STENCIL_STROKES | NVG_DEBUG);
     GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
     
     if ( status == GL_FRAMEBUFFER_COMPLETE ) {
@@ -141,7 +154,7 @@
 {
     glViewport(0, 0, rect.size.width, rect.size.height);
     
-    [self updateBuffersSize];
+    [self updateBuffersSize:rect];
     
     glBindRenderbuffer(GL_RENDERBUFFER, _samplestencilbuffer);
     glBindRenderbuffer(GL_RENDERBUFFER, _samplerenderbuffer);
@@ -149,6 +162,21 @@
     
     glClearColor(1.f, 0.f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    
+    nvgBeginFrame(self.vg, roundf(self.view.frame.size.width), roundf(self.view.frame.size.height), [UIScreen mainScreen].scale);
+    
+    for ( int i = 0; i < 30; i++ ) {
+        int randNum = rand() % (255 - 0) + 0;
+        int randNum2 = rand() % (5 - 0) + 0;
+        nvgStrokeColor(self.vg, nvgRGB(randNum, 0, 0));
+        nvgStrokeWidth(self.vg, randNum2);
+        nvgBeginPath(self.vg);
+        nvgMoveTo(self.vg, 0.0, 0.0 + i * 2);
+        nvgLineTo(self.vg, 100.0, 100.0 + i * 2);
+        nvgStroke(self.vg);
+    }
+    
+    nvgEndFrame(self.vg);
     
     glBindFramebuffer(GL_READ_FRAMEBUFFER_APPLE, _sampleframebuffer);
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER_APPLE, self.view.framebuffer);
