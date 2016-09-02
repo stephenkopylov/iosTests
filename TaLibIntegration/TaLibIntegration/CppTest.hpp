f85e11159e8c72d357e90fc8b90e4884eaed282c
Stephen Kopylov - Home <stephen@expertoption.com>
2016-09-03 00:21:27 +0300

    Some additions

--- TaLibIntegration/TaLibIntegration/CppTest.hpp
+++ TaLibIntegration/TaLibIntegration/CppTest.hpp
@@ -10,11 +10,29 @@
 #define CppTest_hpp
 
 #include <stdio.h>
+#include "ta_func.h"
 
 class CppTest {
 public:
     CppTest(){
+        TA_Real    closePrice[400];
+        TA_Real    out[400];
+        TA_Integer outBeg;
+        TA_Integer outNbElement;
+        /* ... initialize your closing price here... */
         
+        for (int i = 0; i < 400-1; i++) {
+            closePrice[i] = 0.2f;
+        }
+        
+        TA_RetCode retCode = TA_MA( 0, 399,
+                        &closePrice[0],
+                        30,TA_MAType_SMA,
+                        &outBeg, &outNbElement, &out[0] );
+        /* The output is displayed here */
+        for( int i=0; i < outNbElement; i++ ){
+            printf( "Day %d = %f\n", outBeg+i, out[i] );
+        }
     }
 };
 
