6b255788a1e5c547cc4eaaeea664080a6d5406d6
Stephen Kopylov - Home <stephen@expertoption.com>
2016-10-09 21:14:58 +0300

    Some additions

--- ASTableViewUpdatesSample/ASTableViewUpdatesSample/ASTableViewController.m
+++ ASTableViewUpdatesSample/ASTableViewUpdatesSample/ASTableViewController.m
@@ -81,7 +81,7 @@
             [[RLMRealm defaultRealm] commitWriteTransaction];
         }
         
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self update];
         });
     });
